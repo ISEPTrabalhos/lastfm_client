631e4edb7736ce783c873f9b9608609483aff524
Mario Garrido <mrfhitz@gmail.com>
2014-10-05 03:32:07 +0100

    Maybe it will work... maybe not, we will see tomorrow

---
+++ assets/js/homepage.js
@@ -0,0 +1,7 @@
+function changeSearchTextField() {
+    //var table1 = document.getElementById("controlls");
+    //var table2 = document.getElementById("eventtable");
+
+    //table1.object.style.display = (table1.object.style.display == "none") ? "block" : "none";
+    //table2.object.style.display = (table2.object.style.display == "none") ? "block" : "none";
+}
