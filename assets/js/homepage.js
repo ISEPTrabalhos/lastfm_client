07be173a15fd273b072d90ae8f23069a4befab80
Mario Garrido <mrfhitz@gmail.com>
2014-10-05 16:19:15 +0100

    add switch to event search

--- assets/js/homepage.js
+++
@@ -1,7 +0,0 @@
-function changeSearchTextField() {
-    //var table1 = document.getElementById("controlls");
-    //var table2 = document.getElementById("eventtable");
-
-    //table1.object.style.display = (table1.object.style.display == "none") ? "block" : "none";
-    //table2.object.style.display = (table2.object.style.display == "none") ? "block" : "none";
-}
---
+++ assets/js/lastfm_events.js
@@ -0,0 +1,7 @@
+function changeSearchTextField() {
+    var table1 = document.getElementById("controlls");
+    var table2 = document.getElementById("eventtable");
+
+    table1.style.display = (table1.style.display == "none") ? "block" : "none";
+    table2.style.display = (table2.style.display == "none") ? "block" : "none";
+}
