748c8378aa085d0f2b9250ed919a3ce0a221a799
Mario Garrido <mrfhitz@gmail.com>
2014-10-05 17:09:17 +0100

    Add new way to handle results in the css and html

--- assets/js/lastfm_events.js
+++ assets/js/lastfm_events.js
@@ -4,6 +4,11 @@ function changeSearchTextField() {
 
     table1.style.display = (table1.style.display == "none") ? "block" : "none";
     table2.style.display = (table2.style.display == "none") ? "block" : "none";
+
+    // in cleaning duty
+    document.getElementById("divTagTopTracks").innerHTML = "";
+    document.getElementById("divEventsResults").innerHTML = "";
+    document.getElementById("selectTopTag").value = "--";
 }
 
 function getEvents() {
